cce20dd576fde2bbcaf3f6527350be19a6d2b357
ibeneme <ikennaibenemee@gmail.com>
2026-01-24 09:44:43 +0100

    Merge remote changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
-require("dotenv").config(); // load .env variables
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI); // no options needed
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Error:", error.message);
